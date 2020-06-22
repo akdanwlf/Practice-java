9daf5789d89de5e486a5f154472bb8d2eb6a82c5
akdanwlf <akhildandamudi@gmail.com>
2020-06-21 20:38:20 -0400

    Adding exercise 17 files

---
+++ Exercises/exercise17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if ("0".equals(input)) {
+                break;
+            }
+            try {
+                System.out.println(Integer.parseInt(input) * 10);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid user input: " + input);
+            }
+        }
+    }
+}
