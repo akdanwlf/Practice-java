d73ccf9f6354bb2cc56db7786e4ff8ac0714e770
akdanwlf <akhildandamudi@gmail.com>
2020-07-15 20:49:58 -0400

    Adding exercise 21

---
+++ Exercises/ex21/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        final int target = scanner.nextInt();
+        scanner.close();
+        
+        int left = 0;
+        int right = 0;
+        int blockSize = (int) Math.sqrt(array.length);
+
+        while (right < array.length - 1) {
+            right = Math.min(array.length, left + blockSize) - 1;
+            if (array[right] >= target) {
+                System.out.println(left + " " + right);
+                return;
+            }
+            left = right + 1;
+        }
+        System.out.println("-1");
+    }
+}
---
+++ Exercises/ex21/exercise21.md
@@ -0,0 +1,11 @@
+## Find a block that may contain the target value
+
+Write a program that reads an ascending sorted array of integers and a single value. The program should find the first suitable block for this value according to the values of block borders. The algorithm should follow the jump search implementation.
+
+### Sample Input 1:
+10</br>
+0 1 1 1 3 5 8 13 21 34</br>
+1
+
+### Sample Output 1:
+0 2
